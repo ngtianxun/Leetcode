0322d63af3e6098c0134a4a7ab43c7bb9be4bdf8
ngtianxun <luckily_star0826@hotmail.com>
2022-02-21 12:12:34 +0800

    Self coded version.

--- Leetcode_Easy_14_LongestCommonPrefix/src/LongestCommonPrefix.java
+++ Leetcode_Easy_14_LongestCommonPrefix/src/LongestCommonPrefix.java
@@ -29,20 +29,21 @@ public class LongestCommonPrefix {
 //	BBAAA
 	
 	public static String longestCommonPrefix(String[] strs) {
-        // Argument checks
-        if (strs == null || strs.length == 0) return "";
-        if (strs.length == 1) return strs[0];
-        
-        StringBuilder sb = new StringBuilder();
-        Arrays.sort(strs);
-        char[] first = strs[0].toCharArray();
-        char[] last = strs[strs.length - 1].toCharArray();
-        for (int i = 0, j = 0; i < first.length && j < last.length; i++, j++) {
-            if (first[i] != last[j]) break;
-            sb.append(first[i]);
-        }
-        return sb.toString();
-    }
+		StringBuilder commonPrefix = new StringBuilder();
+		Arrays.sort(strs);
+		
+		char[] first = strs[0].toCharArray();
+		char[] last = strs[strs.length - 1].toCharArray();
+		
+		// Compare only the first and last since they are most different from each other.
+		for (int i = 0; i < strs.length; i++) {
+			if (first[i] == last[i]) {
+				commonPrefix.append(first[i]);
+			}
+		}
+		
+		return commonPrefix.toString();
+    	}
 
 	public static void main(String[] args) {
 		String strs[] = {"flo", "flow", "floght"};
